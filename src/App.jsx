29e0a13a50ade6729c3912e4140f0a95da042a21
nghiemtuan2001 <nghiemtuan28112001@gmail.com>
2022-01-10 13:49:18 +0700

    modify files

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,103 @@
-import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import SignUp from "./components/auth/SignUp";
-import SignIn from "./components/auth/SignIn";
-import AppContent from "./components/AppContent";
-import PrivateRoute from "./routes/PrivateRoute";
-import ChangePassword from "./components/auth/ChangePassword";
-import NotFound from "./components/UI/NotFound";
+import React, { useEffect } from "react";
+import { auth } from "./others/firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { Outlet } from "react-router-dom";
+import { fetchUserData } from "./store/auth-actions";
+import { fetchMenuData, putMenuData } from "./store/menu-actions";
+import { fetchCartData, putCartData } from "./store/cart-actions";
+import useToken from "./hooks/use-token";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import HeroBanner from "./components/layout/HeroBanner";
+import Meals from "./components/Meals/Meals";
+import { uiActions } from "./store/ui-slice";
+import Cart from "./components/Cart/Cart";
+import NewMeal from "./components/Meals/NewMeal";
+import CheckoutList from "./components/Meals/CheckoutList";
+import { authActions } from "./store/auth-slice";
 
-function App() {
+const App = () => {
+  const { token, removeToken } = useToken();
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+  const menu = useSelector((state) => state.menu.menu);
+  const isShownCart = useSelector((state) => state.ui.isShownCart);
+  const isAddingNewMealPopup = useSelector((state) => state.ui.isAddingNewMealPopup);
+  const isShownCheckoutList = useSelector((state) => state.ui.isShownCheckoutList);
+  const user = useSelector((state) => state.auth.currentUser);
+
+  // Handler:
+  const handleToggleCart = (bool) => () => {
+    dispatch(uiActions.toggleCart(bool));
+  };
+  const handleToggleAddNewMeal = (bool) => () => {
+    dispatch(uiActions.toggleAddNewMealPopup(bool));
+  };
+  const handleToggleCheckoutList = (bool) => () => {
+    dispatch(uiActions.toggleCheckoutList(bool));
+  };
+  const handleSignOut = async () => {
+    removeToken();
+  };
+  //////////////////////////////////////////////////////////////////////////////////
   useEffect(() => {
-    document.title = "FastFoodz";
-  }, []);
-  return (
-    <Routes>
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/" element={<AppContent />}>
-        <Route
-          path="change-password"
-          element={
-            <PrivateRoute>
-              <ChangePassword />
-            </PrivateRoute>
-          }
-        />
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-}
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(fetchUserData(user));
+      } else dispatch(authActions.updateCurrentUser(null));
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, [dispatch, token]);
+  //////////////////////////////////////////////////////////////////////////////////
+  //Fetch menu data:
+  useEffect(() => {
+    dispatch(fetchMenuData());
+  }, [dispatch]);
+  useEffect(() => {
+    if (menu.length > 0) {
+      dispatch(putMenuData(menu));
+    }
+  }, [dispatch, menu]);
 
-export default App;
+  // Fetch and send cart data:
+  useEffect(() => {
+    if (!user || !token) return;
+    dispatch(fetchCartData(user.uid));
+  }, [dispatch, user, token]);
+  useEffect(() => {
+    if (!user) return;
+    const putDataIndex = setTimeout(() => {
+      dispatch(putCartData(cart, user.uid));
+    }, 500);
+    return () => {
+      clearTimeout(putDataIndex);
+    };
+  }, [dispatch, cart, user]);
+
+  return (
+    <>
+      {isShownCart && <Cart onHideCart={handleToggleCart} />}
+      {isAddingNewMealPopup && user?.role.includes("admin") && (
+        <NewMeal onToggleAddMeal={handleToggleAddNewMeal} />
+      )}
+      {isShownCheckoutList && <CheckoutList onClose={handleToggleCheckoutList} />}
+
+      <Header
+        onToggleCart={handleToggleCart}
+        onSignOut={handleSignOut}
+        onToggleAddMeal={handleToggleAddNewMeal}
+        onToggleCheckoutList={handleToggleCheckoutList}
+      />
+      <main>
+        <HeroBanner />
+        <Meals />
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default React.memo(App);
--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,43 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/store";
 import ReactDOM from "react-dom";
+
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./components/auth/SignUp";
+import SignIn from "./components/auth/SignIn";
+import PrivateRoute from "./routes/PrivateRoute";
+import ChangePassword from "./components/auth/ChangePassword";
+import NotFound from "./components/UI/NotFound";
 import App from "./App";
+
+const AppContent = () => {
+  useEffect(() => {
+    document.title = "FastFoodz";
+  }, []);
+  return (
+    <Routes>
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/" element={<App />}>
+        <Route
+          path="change-password"
+          element={
+            <PrivateRoute>
+              <ChangePassword />
+            </PrivateRoute>
+          }
+        />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <AppContent />
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
